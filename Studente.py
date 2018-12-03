978026601ae09f2d626f1eaf826b918125fa9f94
Sabry18 <43953229+sabrybarbato@users.noreply.github.com>
2018-12-03 11:03:05 +0000

    Update Studente.py

--- Studente.py
+++ Studente.py
@@ -1,8 +1,8 @@
 class studente:
       def __init__ (self,nome,cognome,classe):
-          self.nome=nome
-	  self.cognome=cognome
-	  self.classe=classe
+		self.nome=nome
+		self.cognome=cognome
+		self.classe=classe
 
 
 
@@ -10,11 +10,11 @@ alunni = []
 S=" "
 n=input ("quanti studenti vuoi inserire")
 n=int (n)
-for a in range(n)
+for a in range(n):
  	nome =  input ("inserisci nome")
 	cognome = input ("inserisci cognome")
 	classe = input ("inserisci classe ")
-        S=studente(nome,cognome,classe)
+	S=studente(nome,cognome,classe)
 	alunni.append(S)
 for alunno in alunni:
  	print (alunno.nome)
