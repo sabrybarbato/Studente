9840ddddc4920ce3ed12c8b600fe9c3e456863be
sabrybarbato <sabrymiky12@gmail.com>
2018-12-03 10:36:48 +0000

    esercizio studente

---
+++ Studente.py
@@ -0,0 +1,23 @@
+class studente:
+      def __init__ (self,nome,cognome,classe):
+          self.nome=nome
+	  self.cognome=cognome
+	  self.classe=classe
+
+
+
+alunni = []
+S=" "
+n=input ("quanti studenti vuoi inserire")
+n=int (n)
+for a in range(n)
+ 	nome =  input ("inserisci nome")
+	cognome = input ("inserisci cognome")
+	classe = input ("inserisci classe ")
+        S=studente(nome,cognome,classe)
+	alunni.append(S)
+for alunno in alunni:
+ 	print (alunno.nome)
+	print (alunno.cognome)
+	print (alunno.classe)
+
